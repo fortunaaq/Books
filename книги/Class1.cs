55d9d468c97c5ecb5c90690e123e476f0168dacd
Bogdan <Bogdan@DESKTOP-INFJICI>
2023-06-10 17:42:12 +0500

    Добавьте файлы проекта.

---
+++ книги/Class1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System.Linq;
+using static task15.Книга;
+using System.Xml.Linq;
+
+namespace task15
+{
+    internal class Книга
+    {
+        public abstract class Publication
+        {
+            public string Name { get; set; }                 //свойства
+            public string AuthorLastName { get; set; }
+
+            public Publication(string name, string authorLastName)
+            {
+                Name = name;
+                AuthorLastName = authorLastName;
+            }
+
+            public abstract void DisplayInfo();        //создаем методы
+            public abstract string GiveInfo();
+            public abstract string[] GiveInfo2();
+
+            public abstract bool IsSearchable(string searchQuery);
+            public abstract bool IsSearchable2(string searchQuery);
+            public abstract bool IsSearchable3(string searchQuery);
+
+        }
+        public class Book : Publication
+        {
+            public int Year { get; set; }        //свойства
+            public string Genre { get; set; }
+
+            public Book(string name, string authorLastName, int year, string genre)
+                : base(name, authorLastName)
+            {
+                Year = year;
+                Genre = genre;
+            }
+
+            public override void DisplayInfo()                 //вывести данные книги
+            {
+                Console.WriteLine($"Книга \"{Name}\", автор: {AuthorLastName}, " +
+                                  $"год_издания: {Year}, жанр: {Genre}");
+            }
+            public override string GiveInfo()                  // метод для записи в файл
+            {
+                string info = $" Книга {Name}," + $" автор: {AuthorLastName}," +
+                                  $" год_издания: {Year}," + $" жанр: {Genre}";
+                return info;
+            }
+            public override string[] GiveInfo2()                      // получить данные книги для обработки    
+            {
+                string[] info = new string[4] {Name, AuthorLastName, Convert.ToString(Year), Genre };
+                return info;
+            }
+
+
+            public override bool IsSearchable(string searchQuery)
+            {
+                return AuthorLastName.ToLower().Contains(searchQuery.ToLower());
+            }
+            public override bool IsSearchable2(string searchQuery)
+            {
+                return Genre.ToLower().Contains(searchQuery.ToLower());
+            }
+            public override bool IsSearchable3(string searchQuery)             
+            {
+                return Name.ToLower().Contains(searchQuery.ToLower());
+            }
+        }
+
+            public class Catalog                                   //создаем класс для работы с коллекцией
+        {
+                public List<Publication> publications;
+
+                public Catalog(List<Publication> publications)
+                {
+                    this.publications = publications;
+                }
+
+                public void DisplayAll()                    //выводит все книги из коллекции
+            {
+                    int i = 1;
+                    foreach (Publication publication in publications)
+                    {
+                    Console.Write(" " + i + ". "); publication.DisplayInfo();
+                    i++;
+                    }
+                }
+
+                public void SearchByAuthor(string authorLastName)                //ищет книги по автору
+            {
+                    List<Publication> foundPublications = new List<Publication>();
+
+                    foreach (Publication publication in publications)
+                    {
+                        if (publication.IsSearchable(authorLastName))
+                        {
+                            foundPublications.Add(publication);
+                        }
+                    }
+
+                    if (foundPublications.Count == 0)
+                    {
+                        Console.WriteLine("Ничего не найдено");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Найдено {foundPublications.Count} изданий:");
+                        foreach (Publication publication in foundPublications)
+                        {
+                            publication.DisplayInfo();
+                        }
+                    }
+                }
+                public void SearchByGenre(string genre)            //ищет книги по жанру
+            {
+                    List<Publication> foundPublications = new List<Publication>();
+
+                    foreach (Publication publication in publications)
+                    {
+                        if (publication.IsSearchable2(genre))
+                        {
+                            foundPublications.Add(publication);
+                        }
+                    }
+
+                    if (foundPublications.Count == 0)
+                    {
+                        Console.WriteLine("Ничего не найдено");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Найдено {foundPublications.Count} изданий:");
+                        foreach (Publication publication in foundPublications)
+                        {
+                            publication.DisplayInfo();
+                        }
+                    }
+                }
+                public void SearchByName(string name)            //ищет книгу по её названию
+            {
+                    List<Publication> foundPublications = new List<Publication>();
+
+                    foreach (Publication publication in publications)
+                    {
+                        if (publication.IsSearchable3(name))
+                        {
+                            foundPublications.Add(publication);
+                        }
+                    }
+
+                    if (foundPublications.Count == 0)
+                    {
+                        Console.WriteLine("Ничего не найдено");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Найдено {foundPublications.Count} изданий:");
+                        foreach (Publication publication in foundPublications)
+                        {
+                            publication.DisplayInfo();
+                        }
+                    }
+                }
+                public void DeletePublication(int number)              // удаляет книгу из коллекции
+                {
+                    if (number <= publications.Count & number > 0)
+                        {
+                            publications.RemoveAt(number - 1);
+                        }
+                    else
+                        {
+                            Console.WriteLine("Ошибка такого номера нет!");
+                        }
+                }
+            public void ReadFile(string lines)                              //прочитать данные из файла
+                                                                            //(сначала удаляет текущую коллекцию а потом читает и создает новую)
+            {
+                List<string> InfoForBook = new List<string>();
+                List<Publication> publications1 = new List<Publication>();
+                string word = "";
+                string[] file = File.ReadAllLines(lines);
+                foreach (string line in file)
+                {
+                    string[] split2 = line.Split(',');
+                    foreach (string item in split2)
+                    {
+
+                        string[] split3 = item.Split(' ');
+                        for (int i = 2; i + 1 < split3.Length; i++)
+                        {
+
+                            word += split3[i] + " ";
+                        }
+                        word += split3.Last();
+
+                        InfoForBook.Add(word);
+                        word = "";
+                    }
+
+                    int Year = Int32.Parse(InfoForBook[2]);
+                    publications1.Add(new Book(InfoForBook[0], InfoForBook[1], Year, InfoForBook[3]));
+                    InfoForBook.RemoveAt(0); InfoForBook.RemoveAt(0); InfoForBook.RemoveAt(0); InfoForBook.RemoveAt(0);
+                }
+                publications.Clear();
+                foreach (var item in publications1) 
+                    {
+                    publications.Add(item);
+                    }
+                }
+                public void WriteInfile()                             //записать данные в файл
+            {
+                    string text = "";
+                    foreach (var item in publications)
+                    {
+                        string item1 = item.GiveInfo();
+                        text += item1 + "\n";
+                    }
+                    File.WriteAllText("1.txt", text);
+
+                }
+            public void Sorting()                                                   //сортировка пузырьком по году издания
+                {
+                int k = 0;
+                foreach (Publication publication in publications) { k++; }
+
+                for (int i = 0; i  < k; i++)
+                    {
+                        for (int j = 0; j < k - i - 1; j++)
+                        {
+
+                            if (Int32.Parse(publications[j].GiveInfo2()[2]) < Int32.Parse(publications[j + 1].GiveInfo2()[2]))
+                            {
+                            var temp = publications[j];
+                            publications[j] = publications[j + 1];
+                            publications[j+1] = temp;
+                            }
+                        }
+                    }
+                }
+
+
+
+            }
+
+
+    }
+}
---
+++ книги/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static task15.Книга;
+using System.IO;
+
+namespace task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Publication> publications = new List<Publication>();
+            while (true)
+            {
+                Console.WriteLine("\n" + $"Меню:\n" +
+                                  "     1. Добавить новую книгу в коллекцию.\n" +
+                                  "     2. Удалить книгу из колекции.\n" +
+                                  "     3. Найти книгу по названию.\n" +
+                                  "     4. Сортировка колекции по заданному атрибуту.\n" +
+                                  "     5. Вывести коллекцию.\n" +
+                                  "     6. Чтение или запись коллекции в файл\n" +
+                                  "     7. Выход.\n" + "" 
+                                 );
+                string choise = Console.ReadLine();
+                Console.WriteLine();
+                if (choise == "1")
+                {
+                    string book1 = "";
+                    Console.WriteLine("Введите Название:" ); book1 += Console.ReadLine() + ",";
+                    Console.WriteLine("Введите автора:"); book1 += Console.ReadLine() + ",";
+                    Console.WriteLine("Введите год издания:"); book1 += Console.ReadLine() + ",";
+                    Console.WriteLine("Введите жанр:"); book1 += Console.ReadLine() + ",";
+                    string[] book = book1.Split(',');
+                    int year = Int32.Parse(book[2]);
+                    publications.Add(new Book(book[0], book[1], year, book[3]));
+                }
+                else if (choise == "2")
+                {
+                    Console.WriteLine("Выберите книгу которую хотите удалить: (введите её номер)\n" + "");
+                    Catalog catalog1 = new Catalog(publications);
+                    catalog1.DisplayAll();
+
+                    int number = Int32.Parse(Console.ReadLine());
+
+                    catalog1.DeletePublication(number);
+
+
+                }
+                else if (choise == "3")
+                {
+                    Catalog catalog2 = new Catalog(publications);
+
+                    Console.WriteLine("Введите атрибут для поиска (название, автор или жанр)");   // надо вводить именно атрибут , не цифру
+
+                    string attribute = Console.ReadLine();
+
+                    if (attribute.ToLower() == "автор")
+                    {
+                        Console.WriteLine("введите автора: ");
+                        string attribute2 = Console.ReadLine();
+                        catalog2.SearchByAuthor(attribute2);
+                    }
+                    else if (attribute.ToLower() == "жанр")
+                    {
+                        Console.WriteLine("Введите жанр: ");
+                        string attribute2 = Console.ReadLine();
+                        catalog2.SearchByGenre(attribute2);
+
+                    }
+                    else if (attribute.ToLower() == "название")
+                    {
+
+                        Console.WriteLine("Введите название");
+                        string attribute2 = Console.ReadLine();
+                        catalog2.SearchByName(attribute2);
+                    }
+
+                }
+                else if (choise == "4")
+                {
+                    Catalog catalog2 = new Catalog(publications);
+
+                    catalog2.Sorting();
+                }
+                else if (choise == "5")
+                {
+                    Catalog catalog = new Catalog(publications);
+                    catalog.DisplayAll();
+                }
+                else if (choise == "6")
+                {
+                    Console.WriteLine("что вы хотите?\n" +
+                                      "1. Прочитать файл\n" +
+                                      "2. Записать файл"
+                                     ); string choise1 = Console.ReadLine();
+                    if (choise1 == "1")
+                    {
+                        Catalog catalog1 = new Catalog(publications);
+                        catalog1.ReadFile("1.txt");
+                    }
+                    else if (choise1 == "2") 
+                    {
+                        Catalog catalog1 = new Catalog(publications);
+                        catalog1.WriteInfile();
+                    }
+                    
+                }
+                else if (choise == "7") { break; }
+            }
+        }
+    }
+}
